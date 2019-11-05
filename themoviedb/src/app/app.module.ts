61946c0105a62f7130b662561b6a61e8d3ecd205
Christin John Padinjareveettil <cpadinjareve@vmware.com>
2019-11-05 14:30:52 +0530

    themoviedb second commit

--- themoviedb/src/app/app.module.ts
+++ themoviedb/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
@@ -11,6 +12,7 @@ import { AppService } from './app.service';
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
--- themoviedb/src/app/app.service.ts
+++ themoviedb/src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -12,19 +12,58 @@ const httpOptions = {
 })
 export class AppService {
   private contextRoot: any;
-  constructor(private router:Router, private activatedRoute: ActivatedRoute) {
+  private apikey = '3a94078fb34b772a31d9a1348035bed7';
+  private baseURL = 'https://api.themoviedb.org/3/';
+  
+  constructor(private router:Router, private activatedRoute: ActivatedRoute, private httpService: HttpClient) {
     this.contextRoot = this.getContextRoot();
   }
 
   //Return the context path of the current url
-   getContextRoot() {
+  getContextRoot() {
         let pathPart = window.location.href;
         let path = pathPart.split('/')[3];
         return path.split('?')[0]; // Strip off the paramaters if present
-   }
+  }
 
   interceptError(error) {}
 
-  
+  get(inURL, config = undefined) {
+    let options;
+    if (config){
+        options = config;
+    } else {
+        options = httpOptions;
+    }
+
+    let action = this.httpService.get(inURL, options);
+    return new Promise((resolve, reject) => {
+        action.subscribe(
+            response => resolve(response),
+            (error) => {
+                this.interceptError(error);
+                reject(error);
+            }
+        );
+    });
+  }
+
+  getPopular() {
+    let url = this.baseURL + 'discover/movie?api_key=' + this.apikey + '&sort_by=popularity.desc&language=en-US'
+    return this.get(url);
+  }
+
+  getTrending() {
+    let url = this.baseURL + 'trending/all/day?api_key=' + this.apikey + '&sort_by=popularity.desc&language=en-US'
+    return this.get(url);
+  }
+
+  getNewest() {
+
+  }
+
+  getTopRated() {
+
+  }
 
 }
